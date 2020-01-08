31a4a5c4c218e9caccc6b3a29f44065fe066e2d0
Rohan Biswas <brainhacker550@gmail.com>
2020-01-08 10:33:49 +0530

    updated colors

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 static int topbar = 1;
 
 static const char *fonts[] = {
-	"xos4 Terminus:size=9"
+	"Terminus:size=9"
 };
 static const char *prompt      = "";
 static const char *colors[SchemeLast][2] = {
 /*                  fg         bg       */
 	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel]  = { "#eee8d5", "#268bd2" },
+	[SchemeSel]  = { "#eee8d5", "#586e75" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 static unsigned int lines      = 0;
