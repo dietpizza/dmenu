e133c27095b49948f7e203490061156e47d595b1
Rohan Biswas <brainhacker550@gmail.com>
2020-04-12 00:02:57 +0530

    okay

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 static int topbar = 1;
 
 static const char *fonts[] = {
-	"Terminus:size=9"
+	"xos4 Terminus:size=9"
 };
 static const char *prompt      = "";
 static const char *colors[SchemeLast][2] = {
 /*                  fg         bg       */
 	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel]  = { "#fdf6e3", "#268bd2" },
+	[SchemeSel]  = { "#eee8d5", "#268bd2" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 static unsigned int lines      = 0;
