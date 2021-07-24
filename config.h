0ad24e1813c73fd3ede1481b557e9f6be5b6142a
Rohan Biswas <brainhacker550@gmail.com>
2021-07-24 15:45:07 +0530

    edit config

--- config.h
+++ config.h
@@ -1,16 +1,14 @@
 
 static int topbar = 1;
 
-static const char *fonts[] = {
-	"xos4 Terminus:size=9"
-};
-static const char *prompt      = "";
+static const char *fonts[] = {"Terminus:size=9"};
+static const char *prompt = "Choose:";
 static const char *colors[SchemeLast][2] = {
-/*                  fg         bg       */
-	[SchemeNorm] = { "#839496", "#002b36" },
-	[SchemeSel]  = { "#eee8d5", "#268bd2" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+    /*                  fg         bg       */
+    [SchemeNorm] = {"#839496", "#1b2b34"},
+    [SchemeSel] = {"#1b2b34", "#5fb3b3"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
-static unsigned int lines      = 0;
-static unsigned int lineheight = 20;
+static unsigned int lines = 0;
+static unsigned int lineheight = 24;
 static const char worddelimiters[] = " ";
